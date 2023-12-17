f42b2fc97b053c9d056fa5477cfcb109dd684cfe
Daud <78693745+DaudAimanAlYousup@users.noreply.github.com>
2023-12-17 21:42:05 +0100

    Update GabibboFly.py
    Varie modifiche del codice

--- Console3AS/games/Gabibbo/GabibboFly.py
+++ Console3AS/games/Gabibbo/GabibboFly.py
@@ -27,15 +27,15 @@ x = 300
 y = SCREEN_HEIGHT // 2
 
 #dimensioni del gabibbo 
-w = 60
+w = 55
 h = 45
 
 #funzioni per musica
 pygame.mixer.init()
 pygame.mixer.music.load("salto.mp3")
 pygame.mixer.music.set_volume(4.5)
-spazio = 165   # Spazio tra gli ostacoli
-gravità = 2    # forza con cui il corpo viene spinto verso il basso
+spazio = 180   # Spazio tra gli ostacoli
+gravità = 3    # forza con cui il corpo viene spinto verso il basso
 salto = 8      # potenza per ogni tocco di spazio
 player_speed = 4     #velocità del gabibbo
 
@@ -48,22 +48,22 @@ player_img = pygame.image.load("gabibbo.png")
 player_img = pygame.transform.scale(player_img, (w, h))
 #importo immagine del muro basso 
 muro1_img = pygame.image.load("spadaDOWN.png")
-muro1_img = pygame.transform.scale(muro1_img, (100, SCREEN_HEIGHT))
+muro1_img = pygame.transform.scale(muro1_img, (60, SCREEN_HEIGHT))
 #importo immagine muro alto 
 muro2_img = pygame.image.load("spadaUP.png")
-muro2_img = pygame.transform.scale(muro2_img, (100, SCREEN_HEIGHT))
+muro2_img = pygame.transform.scale(muro2_img, (60, SCREEN_HEIGHT))
 
 #importo sfondo 
 screen_img = pygame.image.load("GerryScotti.png")
 screen_img = pygame.transform.scale(screen_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 #font per la il punteggio(ostacoli evitati e game over)
-game_over_carattere = pygame.font.Font(None, 100)
+game_over_carattere = pygame.font.Font(None, 80)
 score_carattere = pygame.font.Font(None, 36)
 
 
 while running:
-    pygame.time.delay(6) #tempo tra lo spawn di nuovi nemici
+    pygame.time.delay(5) #tempo tra lo spawn di nuovi nemici
 
     for event in pygame.event.get():
         #processo per la chiusura del sistema
@@ -83,15 +83,14 @@ while running:
     y += gravità
     player_rect = pygame.Rect(x, y, w, h)    #funzione che si basa sulla gravità e fa abbassare il gabibbo
 
-    screen.fill("black")                     #colore iniziale sfondo 
                 
     screen.blit(screen_img, (0, 0))          #immagine importata che diventa lo sfondo(Gerry) seguendo i movimenti del player
     screen.blit(player_img, (x, y))          #immagine importata che diventa il player seguendo i suoi movimenti
     for muro_x, muro_height in muri:         #permette di creare i due muri 
         muro1 = pygame.Rect(muro_x , 0, 30, muro_height)   #implementa un rettangolo con cordinate diverse per ogni muro(muro basso)
         muro2 = pygame.Rect(muro_x , muro_height + spazio, 30, SCREEN_HEIGHT - muro_height - spazio)#implementa un rettangolo con cordinate diverse per ogni muro(muro basso)
-        screen.blit(muro1_img, (muro_x , muro_height - SCREEN_HEIGHT))#rettangolo che forma il muro basso 
-        screen.blit(muro2_img, (muro_x , muro_height + spazio))#rettangolo che forma il punto alto 
+        screen.blit(muro1_img, (muro_x - 10 , muro_height - SCREEN_HEIGHT))#rettangolo che forma il muro basso 
+        screen.blit(muro2_img, (muro_x - 10 , muro_height + spazio))#rettangolo che forma il muro alto 
 
         if player_rect.colliderect(muro1) or player_rect.colliderect(muro2):  #prende in considerazione i nemici, se li colpisci finisce il gioco
             game_over_testo = game_over_carattere.render("SEI STATO GABIZZATO!", True, "red")  #true implica la scritta rossa
